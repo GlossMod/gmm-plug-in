45e6e753a9cadc0422bd8ce6be4521cef06686e0
小莫 <xiaomo1433675239@outlook.com>
2023-10-30 14:38:08 +0800

    更新文件

---
+++ GetSpecialFolder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GetSpecialFolder
+{
+    public class Program
+    {
+        // 获取我的文档所在目录
+        public async Task<object> GetMyDocuments(object param)
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+
+        // 获取其他路径
+        public async Task<object> GetOther(object param)
+        {
+            return Environment.GetFolderPath((System.Environment.SpecialFolder)param);
+        }
+    }
+}
